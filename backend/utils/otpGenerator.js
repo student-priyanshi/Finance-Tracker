856c2adfc52235d818922f881971ac9ac6e4ff47
student-priyanshi <161323710+student-priyanshi@users.noreply.github.com>
2025-11-18 21:41:46 -0800

    Delete backend directory

--- backend/utils/otpGenerator.js
+++
@@ -1,9 +0,0 @@
-const generateOTP = () => {
-  return Math.floor(100000 + Math.random() * 900000).toString();
-};
-
-const isOTPExpired = (expiresAt) => {
-  return new Date() > new Date(expiresAt);
-};
-
-module.exports = { generateOTP, isOTPExpired };
